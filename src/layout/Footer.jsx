0eefaedbfcae4fc92e397a2cfe6cd2b6d569a841
thereterry <139209658+thereterry@users.noreply.github.com>
2024-01-26 11:00:51 +0100

    footer css

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { TiSocialFacebook } from "react-icons/ti";
+import { SlSocialInstagram } from "react-icons/sl";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="flex justify-center mt-4">
-        <p>&reg; 2023 Alle rettigheder forbeholdes.</p>
+    <footer className='bg-gray-100 p-4'>
+      <div className="flex justify-between items-center mx-40">
+        <div>
+           <p> © 2023 Gjerrild vandrerhjem. All Rights Reserved.</p>
+        </div>
+        <div className='flex space-x-4'>
+          <p className='text-indigo-700 text-xl'><TiSocialFacebook /></p>
+          <p className='text-red-700 text-xl'>
+          <SlSocialInstagram />
+          </p>
+        
+          </div>
+          
       </div>
     </footer>
   )
